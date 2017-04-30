ac3c14f6edcf7d38451e71c116bde8f308a7aa2f
zhangkuo <769329489@qq.com>
2017-04-30 23:59:13 +0800

    搜索控制器

---
+++ ZKWeibo/Controller/Search/ZKSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKSearchViewController.h
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKBaseViewController.h"
+
+@interface ZKSearchViewController : ZKBaseViewController
+
+@end
---
+++ ZKWeibo/Controller/Search/ZKSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZKSearchViewController.m
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKSearchViewController.h"
+
+@interface ZKSearchViewController ()
+
+@end
+
+@implementation ZKSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
