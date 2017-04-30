d04aac22f8fd42f55678c0638caf270443e07b5d
zhangkuo <769329489@qq.com>
2017-04-30 23:59:44 +0800

    用户控制器

---
+++ ZKWeibo/Controller/User/ZKUserHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKUserHomeViewController.h
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKBaseViewController.h"
+
+@interface ZKUserHomeViewController : ZKBaseViewController
+
+@end
---
+++ ZKWeibo/Controller/User/ZKUserHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZKUserHomeViewController.m
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKUserHomeViewController.h"
+
+@interface ZKUserHomeViewController ()
+
+@end
+
+@implementation ZKUserHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
