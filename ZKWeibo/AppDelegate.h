732cd526a08add2ea95831f5dddd16c340725c3b
zhangkuo <769329489@qq.com>
2017-04-30 20:20:17 +0800

    Initial Commit

---
+++ ZKWeibo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ZKWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/4/30.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
