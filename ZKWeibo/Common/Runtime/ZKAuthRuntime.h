a9d13a34bc4c105e867faed825335c58bcdd3a7d
zhangkuo <769329489@qq.com>
2017-05-05 22:47:03 +0800

    acessToken 实体类

---
+++ ZKWeibo/Common/Runtime/ZKAuthRuntime.h
@@ -0,0 +1,14 @@
+//
+//  ZKAuthRuntime.h
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/5/5.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZKAuthRuntime : NSObject
+
++(BOOL)isAuthCodeOutOfTime;
+@end
---
+++ ZKWeibo/Common/Runtime/ZKAuthRuntime.m
@@ -0,0 +1,19 @@
+//
+//  ZKAuthRuntime.m
+//  ZKWeibo
+//
+//  Created by 张阔 on 2017/5/5.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKAuthRuntime.h"
+
+@implementation ZKAuthRuntime
+
++(BOOL)isAuthCodeOutOfTime{
+
+   
+    return YES;
+}
+
+@end
