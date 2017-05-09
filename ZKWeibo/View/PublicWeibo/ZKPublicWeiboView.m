bd98218cf5064ecb3309bc771ce55c4a9402cb25
zhangkuo <769329489@qq.com>
2017-05-09 23:02:50 +0800

    更换 首页 布局

--- ZKWeibo/View/PublicWeibo/ZKPublicWeiboView.m
+++ ZKWeibo/View/PublicWeibo/ZKPublicWeiboView.m
@@ -8,14 +8,15 @@
 
 #import "ZKPublicWeiboView.h"
 #import "ZKPublicWeiboItem.h"
+#import "ZKWeiboPhotoContainer.h"
 
 NSString *const kZKHomeViewID = @"ZKHomeViewID";
 @interface ZKPublicWeiboView ()
 
 @property (strong, nonatomic) UIScrollView *scrollView;
 @property (strong, nonatomic) UIView *contentView;
-@property (strong, nonatomic) UIImageView *coverView;
-@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) ZKWeiboPhotoContainer *photosView;
+@property (strong, nonatomic) UILabel *sourceLabel;
 @property (strong, nonatomic) UILabel *dateLabel;
 @property (strong, nonatomic) UITextView *contentTextView;
 
@@ -96,68 +97,7 @@ NSString *const kZKHomeViewID = @"ZKHomeViewID";
         
         view;
     });
-    _coverView = ({
-        UIImageView *imageView = [UIImageView new];
-        imageView.backgroundColor = [UIColor whiteColor];
-        imageView.contentMode = UIViewContentModeScaleAspectFit;
-        imageView.clipsToBounds = YES;
-        imageView.userInteractionEnabled = YES;
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(coverTapped)];
-        [imageView addGestureRecognizer:tap];
-        [_contentView addSubview:imageView];
-        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.top.right.equalTo(_contentView).insets(UIEdgeInsetsMake(6, 6, 0, 6));
-            make.height.equalTo(imageView.mas_width).multipliedBy(0.75);
-        }];
-        
-        imageView;
-    });
-    _titleLabel = ({
-        UILabel *label = [UILabel new];
-        label.backgroundColor = [UIColor whiteColor];
-        label.textColor = ZKGrayTextColor;
-        label.font = FontWithSize(10);
-        label.textAlignment = NSTextAlignmentRight;
-        [label setContentHuggingPriority:251 forAxis:UILayoutConstraintAxisHorizontal];
-        [_contentView addSubview:label];
-        [label mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(_coverView.mas_bottom).offset(8);
-            make.right.equalTo(_coverView);
-        }];
-        
-        label;
-    });
     
-    _contentTextView = ({
-        UITextView *textView = [UITextView new];
-        textView.backgroundColor = [UIColor whiteColor];
-        textView.textColor = ZKLightBlackTextColor;
-        textView.font = FontWithSize(14);
-        textView.editable = NO;
-        [_contentView addSubview:textView];
-        [textView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(_titleLabel.mas_bottom).offset(15);
-            make.left.right.equalTo(_coverView);
-            _textViewHeightConstraint = make.height.equalTo(@0);
-        }];
-        
-        textView;
-    });
-    
-    _dateLabel = ({
-        UILabel *label = [UILabel new];
-        label.backgroundColor = [UIColor whiteColor];
-        label.textColor = ZKDarkGrayTextColor;
-        label.font = FontWithSize(12);
-        [_contentView addSubview:label];
-        [label mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(_contentTextView.mas_bottom).offset(10);
-            make.right.equalTo(_coverView);
-            make.bottom.equalTo(_contentView).offset(-12);
-        }];
-        
-        label;
-    });
     _userAvatar = ({
         UIImageView *imageView = [UIImageView new];
         imageView.backgroundColor = [UIColor whiteColor];
@@ -168,9 +108,9 @@ NSString *const kZKHomeViewID = @"ZKHomeViewID";
         [imageView addGestureRecognizer:tap];
         [_contentView addSubview:imageView];
         [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.size.sizeOffset(CGSizeMake(25, 25));
-            make.top.equalTo(_contentTextView.mas_bottom).offset(10);
-            make.left.equalTo(_coverView);
+            make.size.sizeOffset(CGSizeMake(40, 40));
+            make.top.equalTo(_contentView.mas_top).offset(10);
+            make.left.equalTo(_contentView).offset(10);
         }];
         
         imageView;
@@ -184,21 +124,81 @@ NSString *const kZKHomeViewID = @"ZKHomeViewID";
         [label addGestureRecognizer:tap];
         [_contentView addSubview:label];
         [label mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(_contentTextView.mas_bottom).offset(16);
+            make.top.equalTo(_contentView.mas_top).offset(12);
             make.left.equalTo(_userAvatar.mas_right).offset(10);
         }];
         
         label;
     });
+    _sourceLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+        label.textColor = ZKGrayTextColor;
+        label.font = FontWithSize(10);
+        label.textColor=[UIColor redColor];
+        label.textAlignment = NSTextAlignmentRight;
+        [label setContentHuggingPriority:251 forAxis:UILayoutConstraintAxisHorizontal];
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_userName.mas_bottom).offset(5);
+            make.left.equalTo(_userAvatar.mas_right).offset(15);
+        }];
+        
+        label;
+    });
+    
+    _dateLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+        label.textColor = ZKDarkGrayTextColor;
+        label.font = FontWithSize(12);
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_contentView.mas_top).offset(16);
+            make.right.equalTo(_contentView).offset(-10);
+        }];
+        
+        label;
+    });
+    
+    _contentTextView = ({
+        UITextView *textView = [UITextView new];
+        textView.backgroundColor = [UIColor whiteColor];
+        textView.textColor = ZKLightBlackTextColor;
+        textView.font = FontWithSize(14);
+        textView.editable = NO;
+        [_contentView addSubview:textView];
+        [textView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_userAvatar.mas_bottom).offset(15);
+            make.left.equalTo(_contentView).offset(10);
+            make.right.equalTo(_contentView).offset(-10);
+            _textViewHeightConstraint = make.height.equalTo(@0);
+        }];
+        
+        textView;
+    });
+    
+    _photosView = ({
+        ZKWeiboPhotoContainer *imageView = [ZKWeiboPhotoContainer new];
+        [_contentView addSubview:imageView];
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_contentTextView.mas_bottom);
+            make.left.right.bottom.equalTo(_contentView).insets(UIEdgeInsetsMake(6, 6, 0, 6));
+            
+        }];
+        
+        imageView;
+    });
+
     
 }
 
 #pragma mark - Action
 
 
-- (void)coverTapped {
-    [self blowUpImage:_coverView.image referenceRect:_coverView.frame referenceView:_coverView.superview];
-}
+//- (void)coverTapped {
+//    [self blowUpImage:_coverView.image referenceRect:_coverView.frame referenceView:_coverView.superview];
+//}
 
 - (void)coverUserAvatarTapped {
     
@@ -214,13 +214,21 @@ NSString *const kZKHomeViewID = @"ZKHomeViewID";
 - (void)configureViewWithHomeItem:(ZKPublicWeiboItem *)publicWeiboItem atIndex:(NSInteger)index inViewController:(ZKBaseViewController *)parentViewController {
     self.viewIndex = index;
     self.parentViewController = parentViewController;
-    [_coverView zk_sd_setImageWithURL:publicWeiboItem.pictures placeholderImageName:@"home_cover_placeholder" cachePlachoderImage:NO];
+//    [_coverView zk_sd_setImageWithURL:publicWeiboItem.pictures placeholderImageName:@"home_cover_placeholder" cachePlachoderImage:NO];
+    [_photosView setPicPathStringsArray:publicWeiboItem.pictures];
+    [_photosView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_contentTextView.mas_bottom);
+        make.height.equalTo(_photosView.mas_height).multipliedBy(0.75);
+        make.left.right.bottom.equalTo(_contentView).insets(UIEdgeInsetsMake(6, 6, 0, 6));
+        
+    }];
+
     _contentTextView.attributedText = [ZKUtilities zk_attributedStringWithText:publicWeiboItem.content lineSpacing:ZKLineSpacing font:_contentTextView.font textColor:_contentTextView.textColor];
     
     _textViewHeightConstraint.equalTo(@(ceilf([ZKUtilities zk_rectWithAttributedString:_contentTextView.attributedText size:CGSizeMake((SCREEN_WIDTH - 24 - 12), CGFLOAT_MAX)].size.height) + 50));
 
     _scrollView.contentOffset = CGPointZero;
-    _titleLabel.text = [ZKUtilities stringSourceWithA:publicWeiboItem.source];
+    _sourceLabel.text = [ZKUtilities stringSourceWithA:publicWeiboItem.source];
     _dateLabel.text = [ZKUtilities stringDateForCommentDate:publicWeiboItem.created_time];
 
     [_userAvatar zk_sd_setImageWithURL:publicWeiboItem.user.avatar placeholderImageName:@"home_cover_placeholder" cachePlachoderImage:NO];
